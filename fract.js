197092db03fd6fe48cb3ae4f496a4a5da013683f
cburch <cody.burchell@gmail.com>
2014-05-24 02:23:37 -0500

    Click for random
    Clicking on the canvas randomizes colors

--- fract.js
+++ fract.js
@@ -3,11 +3,7 @@ var canvas,
     iterSlider,
     redSlider,
     greenSlider,
-    blueSlider,
-    red,
-    green,
-    blue,
-    depth;
+    blueSlider;
 
 function init(){
     canvas = document.getElementById("canvasFract");
@@ -26,29 +22,41 @@ function init(){
     blueSlider.min = 0; blueSlider.max = 255; blueSlider.value = 0;
     
     clearCanvas();
-    setColor(200,0,0);
-    draw(Math.round(canvas.width/3),0,0,0,6);
+    setColor();
+    draw(Math.round(canvas.width/3),0,0,0,iterSlider.value,false);
 }
 
-function draw(l, x, y, de, dl){
+function draw(l, x, y, de, dl, rand){
+    
+    if (rand) {
+        setrandomColor();
+    }
     ctx.fillRect(x+l,y+l,l,l);
     
     var nl = Math.round(l/3);
     
     if (de<dl) {
-        draw(nl,x       ,y         ,de+1,dl);
-        draw(nl,x+l     ,y         ,de+1,dl);
-        draw(nl,x+(l*2) ,y         ,de+1,dl);
-        draw(nl,x       ,y+l       ,de+1,dl);
-        draw(nl,x+(l*2) ,y+l       ,de+1,dl);
-        draw(nl,x       ,y+(l*2)   ,de+1,dl);
-        draw(nl,x+l     ,y+(l*2)   ,de+1,dl);
-        draw(nl,x+(l*2) ,y+(l*2)   ,de+1,dl);
+        draw(nl,x       ,y         ,de+1,dl,rand);
+        draw(nl,x+l     ,y         ,de+1,dl,rand);
+        draw(nl,x+(l*2) ,y         ,de+1,dl,rand);
+        draw(nl,x       ,y+l       ,de+1,dl,rand);
+        draw(nl,x+(l*2) ,y+l       ,de+1,dl,rand);
+        draw(nl,x       ,y+(l*2)   ,de+1,dl,rand);
+        draw(nl,x+l     ,y+(l*2)   ,de+1,dl,rand);
+        draw(nl,x+(l*2) ,y+(l*2)   ,de+1,dl,rand);
     }
 }
 
-function setColor(r,g,b) {
-    ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+function setColor() {
+    ctx.fillStyle = "rgb(" + redSlider.value + "," + greenSlider.value + "," + blueSlider.value + ")";
+}
+
+function setrandomColor(){
+    red = Math.floor(Math.random()*255);
+    green = Math.floor(Math.random()*255);
+    blue = Math.floor(Math.random()*255);
+    
+    ctx.fillStyle = "rgb(" + red + "," + green + "," + blue + ")";
 }
 
 function clearCanvas() {
@@ -57,13 +65,13 @@ function clearCanvas() {
 }
 
 function renderButtonPressed() {
-    red = redSlider.value;
-    green = greenSlider.value;
-    blue = blueSlider.value;
-    depth = iterSlider.value;
-    
     clearCanvas();
     
-    setColor(red,green,blue);
-    draw(Math.round(canvas.width/3),0,0,0,depth);
+    setColor();
+    draw(Math.round(canvas.width/3),0,0,0,iterSlider.value,false);
+}
+
+function clicked(){
+    clearCanvas();
+    draw(Math.round(canvas.width/3),0,0,0,iterSlider.value,true);
 }
